bfd5ddb441f06dc6af4fe7b1ff2676671f56be36
Vladyslav Mishchenko <mishchenko.online@gmail.com>
2024-10-13 00:10:13 +0300

    Add footer subscribe form html

--- src/js/test.js
+++ src/js/test.js
@@ -1,1 +1,8 @@
-console.log('my js');
+const form = document.querySelector('.footer-subscribe-form');
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+    
+    console.log(form["name"].value);
+    console.log(form["email"].value);
+});
