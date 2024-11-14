64ee7ad3113b0ec95ce45482f1876e46a7eb42b6
Vladyslav Mishchenko <mishchenko.online@gmail.com>
2024-11-14 19:21:40 +0200

    Refactoring form preloaders and mobile menu overlay

--- src/js/subscribe.js
+++ src/js/subscribe.js
@@ -1,5 +1,5 @@
 const form = document.querySelector('.footer-subscribe-form');
-const footerSubscribe = document.querySelector('#fs');
+const formPreloaderOverlay = document.querySelector('#fs > .form-preloader-overlay');
 const preloaderModes = document.querySelectorAll('.preloader-modes .mode');
 
 if(form != null){
@@ -7,9 +7,9 @@ if(form != null){
     form.addEventListener('submit', function(e){
         e.preventDefault();
         
-        footerSubscribe.classList.add('d');
+        formPreloaderOverlay.classList.add('d');
         setTimeout(function(){
-            footerSubscribe.classList.add('a');
+            formPreloaderOverlay.classList.add('a');
 
             setTimeout(function(){
                 preloaderModes[0].classList.remove('on');
